7c7b1f78dd389fb44ead4600846f421139ddaeda
Milad Irannejad <moorara@users.noreply.github.com>
2018-12-15 00:58:55 -0500

    Add test package (#12)

--- cmd/command/test.go
+++ cmd/command/test.go
@@ -1,7 +1,11 @@
 package command
 
 import (
+	"context"
+	"time"
+
 	"github.com/mitchellh/cli"
+	"github.com/moorara/cherry/internal/test"
 )
 
 const (
@@ -14,29 +18,41 @@ const (
 type (
 	// Test is the test CLI command
 	Test struct {
-		ui cli.Ui
+		ui     cli.Ui
+		tester test.Tester
 	}
 )
 
 // NewTest create a new test command
 func NewTest(ui cli.Ui) *Test {
+	tester := test.NewTester("./")
+
 	return &Test{
-		ui: ui,
+		ui:     ui,
+		tester: tester,
 	}
 }
 
 // Synopsis returns the short one-line synopsis of the command.
-func (b *Test) Synopsis() string {
+func (c *Test) Synopsis() string {
 	return testSynopsis
 }
 
 // Help returns the long help text including usage, description, and list of flags for the command
-func (b *Test) Help() string {
+func (c *Test) Help() string {
 	return testHelp
 }
 
 // Run runs the actual command with the given CLI instance and command-line arguments
-func (b *Test) Run(args []string) int {
-	b.ui.Output("test command run")
+func (c *Test) Run(args []string) int {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := c.tester.Coverage(ctx)
+	if err != nil {
+		c.ui.Error(err.Error())
+		return 1
+	}
+
 	return 0
 }
